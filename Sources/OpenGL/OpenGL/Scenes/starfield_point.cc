7dc2fca66813064fdb40debf1d2d7ea8c4b649f1
Neu Yun <vertx.ngng@gmail.com>
2018-01-15 18:41:52 +0900

    Add startfield class (under construction)

---
+++ Sources/OpenGL/OpenGL/Scenes/starfield_point.cc
@@ -0,0 +1,15 @@
+#include "starfield_point.h"
+
+StarField::StarField() :
+    font{ "Resources/LSANS.TTF" } {}
+
+void StarField::InitShaders() {}
+
+void StarField::HandleInput(GLFWwindow * const window) {
+    Scene::HandleInput(window);
+}
+
+void StarField::Draw() {}
+
+void StarField::Update() {}
+
---
+++ Sources/OpenGL/OpenGL/Scenes/starfield_point.h
@@ -0,0 +1,45 @@
+#ifndef OPENGL_TUTORIAL_SCENES_STARFIELD_POINT_H
+#define OPENGL_TUTORIAL_SCENES_STARFIELD_POINT_H
+#include "..\object.h"
+#include "..\shader.h"
+#include "..\scene.h"
+
+class StarField : public Scene {
+public:
+    StarField();
+    virtual ~StarField() = default;
+
+    /**
+     * @brief The method handles key codes from window.
+     *
+     * This methods is called by Application::ProcessInput method.
+     *
+     * @param[in] window Window handle pointer.
+     */
+    [[noreturn]] virtual void HandleInput(GLFWwindow* const window) override;
+
+    /**
+     * @brief The method update components movement, UI refresh, and so on.
+     */
+    [[noreturn]] virtual void Update() override;
+
+    /**
+     * @brief The method calls scene to draw all objects.
+     */
+    [[noreturn]] virtual void Draw() override;
+
+private:
+    Font font;
+    
+    /** ----------------------------------------------------------------------+
+     * @comment Initiate methods
+     */
+
+    /**
+     * @brief Initiate shaders to use in this scene.
+     */
+    [[noreturn]] void InitShaders();
+};
+
+
+#endif /** OPENGL_TUTORIAL_SCENES_STARFIELD_POINT_H */
