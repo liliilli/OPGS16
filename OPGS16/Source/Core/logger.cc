dfe059acdde0df3813e89037cc887c1aab22de2a
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-05-23 00:50:34 +0900

    fact: Add 181c7d2

--- OPGS16/Source/Core/logger.cc
+++ OPGS16/Source/Core/logger.cc
@@ -5,81 +5,71 @@
 /// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
 /// If you want to read full statements, read LICENSE file.
 ///
-
-///
 /// @file Core/logger.h
-/// @brief CLogger class implementation file.
+///
+/// @brief 
+/// CLogger class implementation file.
+///
 /// @author Jongmin Yun
+///
 /// @log
 /// 2018-03-06 Add logger implementation file.
 /// 2018-04-02 const char* to const wchar_t* for UTF-16 text.
+/// 2018-05-22 Refactored deform class structure and recreated to namespace.
+/// 2018-05-23 Removed wchar_t parameter version of function.
 ///
 
-#include <Core\logger.h>            /// Header file
-#include <Core\Impl\logger_impl.h>  /// ::opgs16::debug::_internal::CLoggerImpl
+#include <Core\logger.h>  /// Header file
 
-namespace opgs16 {
-namespace debug {
+#include <memory>
 
-CLogger::CLogger() :
-    m_impl { std::make_unique<_internal::CLoggerImpl>() } {
+/// Enhanced assertion
+#include <Helper\assert.h>
 
-    auto& m_console = m_impl->Logger();
-    using _internal::MsgType;
-    Push(MsgType::INFO, L"Welcome to spdlog Hello world.");
-    Push(MsgType::WARN, L"This is warning message...");
-    Push(MsgType::_ERROR, L"An info message example 1..");
+/// ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
+/// Member container
+/// ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
+
+namespace {
+  
+std::shared_ptr<spdlog::logger> m_console = spdlog::stdout_color_mt("OPGS16");
+
+} /// unnamed namespace
+
+/// ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
+/// Definition part
+/// ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
+
+namespace opgs16::debug::log {
+
+void Push(_internal::ELogMessageType msg_type, const char* log_message) {
+  using _internal::ELogMessageType;
+
+  switch (msg_type) {
+  case ELogMessageType::Info: 
+    m_console->info(log_message);
+    break;
+  case ELogMessageType::Debug: 
+    m_console->debug(log_message);
+    break;
+  case ELogMessageType::Warning:
+    m_console->warn(log_message);
+    break;
+  case ELogMessageType::Critical:
+    m_console->critical(log_message);
+    break;
+  case ELogMessageType::Error: 
+    m_console->error(log_message);
+    break;
+  }
 }
 
-void CLogger::Push(_internal::MsgType type, const wchar_t* log_message) {
-    using _internal::MsgType;
-    switch (type) {
-    case MsgType::INFO: m_impl->Logger().info(log_message);
-        break;
-    case MsgType::WARN: m_impl->Logger().warn(log_message);
-        break;
-    case MsgType::_ERROR: m_impl->Logger().error(log_message);
-        break;
-    default: /*! Do nothing */ break;
-    }
+} /// ::opgs16::debug::log
+
+namespace opgs16::debug::log::____ {
+
+std::weak_ptr<spdlog::logger> Get() {
+  return m_console;
 }
 
-void CLogger::Push(_internal::MsgType type, const char* log_message) {
-    using _internal::MsgType;
-    switch (type) {
-    case MsgType::INFO: m_impl->Logger().info(log_message);
-        break;
-    case MsgType::WARN: m_impl->Logger().warn(log_message);
-        break;
-    case MsgType::_ERROR: m_impl->Logger().error(log_message);
-        break;
-    default: /*! Do nothing */ break;
-    }
-}
-
-CLogger::~CLogger() = default;
-
-void PushLog(_internal::MsgType type, const wchar_t* log_message) {
-    CLogger::Instance().Push(type, log_message);
-}
-
-void PushLog(LOG_TYPE type, const wchar_t* log_message) {
-    switch (type) {
-    case LOG_TYPE_INFO: CLogger::Instance().Push(_internal::MsgType::INFO, log_message); break;
-    case LOG_TYPE_WARN: CLogger::Instance().Push(_internal::MsgType::WARN, log_message); break;
-    case LOG_TYPE_ERRO: CLogger::Instance().Push(_internal::MsgType::_ERROR, log_message); break;
-    default: break;
-    }
-}
-
-void PushLog(LOG_TYPE type, const char* log_message) {
-    switch (type) {
-    case LOG_TYPE_INFO: CLogger::Instance().Push(_internal::MsgType::INFO, log_message); break;
-    case LOG_TYPE_WARN: CLogger::Instance().Push(_internal::MsgType::WARN, log_message); break;
-    case LOG_TYPE_ERRO: CLogger::Instance().Push(_internal::MsgType::_ERROR, log_message); break;
-    default: break;
-    }
-}
-
-} /*! opgs16::debug */
-} /*! opgs16 */
+} /// ::opgs16::debug::log::____
