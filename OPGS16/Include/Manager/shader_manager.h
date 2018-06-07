55269e82149b30f3120f450b76f9d669ed087dff
liliilli <jmyundev@gmail.com>
2018-06-07 14:50:20 +0900

    refc: 1st refactoring, move to namespace

--- OPGS16/Include/Manager/shader_manager.h
+++ OPGS16/Include/Manager/shader_manager.h
@@ -1,155 +1,89 @@
 #ifndef OPGS16_SYSTEM_SHADER_SHADER_MANAGER_H
 #define OPGS16_SYSTEM_SHADER_SHADER_MANAGER_H
 
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*!
- * @file System\Shader\shader_manager.h
- * @brief Shader management class.
- *
- * @author Jongmin Yun
- * @log
- * 2018-03-04 Refactoring.
- * 2018-03-11 Correction of object namespace position and path.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Manager/shader_manager.h
+///
+/// @brief
+/// Shader management class.
+///
+/// @author Jongmin Yun
+///
+/// @log
+/// 2018-03-04 Refactoring.
+/// 2018-03-11 Correction of object namespace position and path.
+/// 2018-06-07 Detach class structure to namespace.
+///
 
 #include <memory>
 #include <unordered_map>
 
 /// ::opgs16::manager::resource_manager
-#include <Manager\resource_manager.h>
+#include <Manager/resource_manager.h>
 /// ::opgs16::element::CObject
-#include <Element\object.h>
+#include <Element/object.h>
 /// ::opgs16::element::CShaderNew
-#include <Shader\shader.h>
+#include <Shader/shader.h>
 
-namespace opgs16 {
-namespace manager {
+///
+/// @namespace opgs16::manager::shader
+/// @brief
+/// manages shaders on runtime in scene.
+///
+/// ShaderManager class manages shaders compiled on runtime in scene, These are able to bind
+/// the object in use, render binded m_object_list, move binded object to another shader, and be deleted.
+///
+/// Shaders in container are managed by shader name, and Tags.
+/// ShaderManager is singleton, so user cannot create another ShaderManager instance.
+///
+/// @author Jongmin Yun
+/// @log
+/// 2018-02-06 Create file and make basic features.
+/// 2018-03-04 Refactoring.
+/// 2018-06-07 Detach class structure to namespace.
+///
+///@todo Implement ReleaseAll(), ReleaseShader(), CheckError();
+///
+namespace opgs16::manager::shader {
 
-/*!
- * @class ShaderManager
- * @brief ShaderManager class manages shaders on runtime in scene.
- *
- * ShaderManager class manages shaders compiled on runtime in scene, These are able to bind
- * the object in use, render binded m_object_list, move binded object to another shader, and be deleted.
- *
- * Shaders in container are managed by shader name, and Tags.
- * ShaderManager is singleton, so user cannot create another ShaderManager instance.
- *
- * @author Jongmin Yun
- * @log
- * 2018-02-06 Create file and make basic features.
- * 2018-03-04 Refactoring.
- *
- * @todo Implement ReleaseAll(), ReleaseShader(), CheckError();
- */
-class ShaderManager final {
-public:
-	using shader_raw = element::CShaderNew*;
-	using shader_ptr = std::unique_ptr<element::CShaderNew>;
-    using shader_map = std::unordered_map<std::string, shader_ptr>;
+using shader_raw = element::CShaderNew*;
+using shader_ptr = std::unique_ptr<element::CShaderNew>;
+using shader_map = std::unordered_map<std::string, shader_ptr>;
 
-	/*! Return singleton instance of ShaderManager class. */
-	static ShaderManager& Instance() {
-		static ShaderManager instance{};
-		return instance;
-	}
+///
+/// @brief
+/// Initiate shader manager.
+///
+void Initiate();
 
-	/**
-	 * @brief The method returns arbitary shader which has tag name.
-	 * If the method fails to search shader has tag name, create shader and return.
-	 *
-	 * @param[in] name The tag to find.
-	 * @return Founded shader pointer, if fails create shader and return late.
-	 */
-	inline shader_raw Shader(const std::string& name);
+///
+/// @brief
+/// Release all shader programs and information.
+///
+void ReleaseAll();
 
-	/** Release all shader program in shader container. */
-	void Clear();
+///
+/// @brief
+///
+///
+/// @param[in] shader_name The name tag to release.
+///
+void ReleaseShader(const std::string& shader_name);
 
-	/**
-	 * @brief
-	 * @param[in] shader_name The name tag to release.
-	 */
-	void ReleaseShader(const std::string& shader_name);
+///
+/// @brief The method returns arbitary shader which has tag name.
+/// If the method fails to search shader has tag name, create shader and return.
+///
+/// @param[in] name The tag to find.
+/// @return Founded shader pointer, if fails create shader and return late.
+///
+inline shader_raw GetShader(const std::string& name);
 
-private:
-	/**
-	 * @brief Override version method of CreateShader(std::string, shader_list).
-	 * @param[in] tag The tag to specify.
-	 * @param[in] list vector conatiner contains shader arguments to create.
-	 * @return created shader raw-pointer.
-	 */
-	shader_raw CreateShader(const std::string& tag, const opgs16::resource::SShader& container);
+} /// ::opgs16::manager namespace
 
-private:
-	/** ShaderManager error type */
-	enum class ErrorType {
-		OK,
-	} m_error{ ErrorType::OK };
-
-	shader_map m_shaders{};	// Shader container.
-
-private:
-	/**
-	 * @brief Check and return true / false whether or not Shader with name is already initiated.
-	 * @param[in] shader_name Shader name to search.
-	 * @return If shader already exist, return true else false.
-	 */
-	inline bool DoesShaderExist(const std::string& shader_name) {
-		return m_shaders.find(shader_name) != m_shaders.end();
-	}
-
-private:
-	ShaderManager();
-
-public:
-	ShaderManager(const ShaderManager&) = delete;
-	ShaderManager& operator=(const ShaderManager&) = delete;
-};
-
-/**
- * @brief The method returns arbitary shader which has tag name.
- * If the method fails to search shader has tag name, create shader and return.
- *
- * @param[in] name The tag to find.
- * @return Founded shader pointer, if fails create shader and return late.
- */
-inline ShaderManager::shader_raw ShaderManager::Shader(const std::string& name) {
-	if (!DoesShaderExist(name)) {
-		auto& list = manager::resource::GetShader(name);
-		CreateShader(name, list);
-	}
-	return m_shaders[name].get();
-}
-
-} /*! opgs16::manager */
-} /*! opgs16 */
-
-#endif /** OPGS16_SYSTEM_SHADER_SHADER_MANAGER_H */
+#endif /// OPGS16_SYSTEM_SHADER_SHADER_MANAGER_H
--- OPGS16/Source/Manager/shader_manager.cc
+++ OPGS16/Source/Manager/shader_manager.cc
@@ -1,90 +1,120 @@
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-/*!
- * @file System/Manager/Private/shader_manager.cc
- * @brief ShaderManager implementation file.
- *
- * @author Jongmin Yun
- * @log
- * 2018-03-04 Refactoring. Add copyright and comments.
- * 2018-04-03 Add default quad2d(gQuad) shader in bootstrapping.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+///
+/// @file Manager/shader_manager.cc
+/// @brief ShaderManager implementation file.
+///
+/// @author Jongmin Yun
+/// @log
+/// 2018-03-04 Refactoring. Add copyright and comments.
+/// 2018-04-03 Add default quad2d(gQuad) shader in bootstrapping.
+///
 
-#include <Manager\shader_manager.h> /// Header file
+/// Header file
+#include <Manager/shader_manager.h>
 
-/// opgs16::builtin::shader::SGlobalQuad2D 
-#include <Shader\Default\shader_quad2d.h>
+/// opgs16::builtin::shader::SGlobalQuad2D
+#include <Shader/Default/shader_quad2d.h>
 /// opgs16::builtin::shader::SGlobalFont
-#include <Shader\Default\shader_font.h>
+#include <Shader/Default/shader_font.h>
 /// opgs16::builtin::shader::SPostProcessPlainQuad
-#include <Shader\Default\shader_postprocess_plain_quad.h>
+#include <Shader/Default/shader_postprocess_plain_quad.h>
 
-#include <Headers\import_logger.h>  /// import logger
+/// import logger
+#include <Headers/import_logger.h>
 
-namespace opgs16::manager {
+//!
+//! Forward declaration.
+//!
 
-ShaderManager::ShaderManager() {
-    using namespace builtin::shader;
-    m_shaders[SGlobalQuad2D::s_shader_name] = std::make_unique<SGlobalQuad2D>();
-    m_shaders[SGlobalQuad2D::s_shader_name]->Link();
-    m_shaders[SGlobalFont2D::s_shader_name] = std::make_unique<builtin::shader::SGlobalFont2D>();
-    m_shaders[SGlobalFont2D::s_shader_name]->Link();
-    m_shaders[SGlobalPostProcessingQuad::s_shader_name] =
-        std::make_unique<builtin::shader::SGlobalPostProcessingQuad>();
-    m_shaders[SGlobalPostProcessingQuad::s_shader_name]->Link();
+///
+/// @brief Override version method of CreateShader(std::string, shader_list).
+/// @param[in] tag The tag to specify.
+/// @param[in] container vector conatiner contains shader arguments to create.
+/// @return created shader raw-pointer.
+///
+opgs16::manager::shader::shader_raw
+    CreateShader(const std::string& tag,
+                 const opgs16::resource::SShader& container);
+
+///
+/// @brief
+/// Check and return true / false whether or not Shader with name is already initiated.
+///
+/// @param[in] shader_name Shader name to search.
+///
+/// @return If shader already exist, return true else false.
+///
+inline bool DoesShaderExist(const std::string& shader_name);
+
+//!
+//! Data
+//!
+
+namespace {
+// Shader container.
+opgs16::manager::shader::shader_map m_shaders{};
+} /// unnamed namespace
+
+//!
+//! Definitions.
+//!
+
+namespace opgs16::manager::shader {
+
+void Initiate() {
+  using namespace builtin::shader;
+
+  m_shaders[SGlobalQuad2D::s_shader_name] = std::make_unique<SGlobalQuad2D>();
+  m_shaders[SGlobalQuad2D::s_shader_name]->Link();
+  m_shaders[SGlobalFont2D::s_shader_name] = std::make_unique<SGlobalFont2D>();
+  m_shaders[SGlobalFont2D::s_shader_name]->Link();
+  m_shaders[SGlobalPostProcessingQuad::s_shader_name] =
+    std::make_unique<SGlobalPostProcessingQuad>();
+  m_shaders[SGlobalPostProcessingQuad::s_shader_name]->Link();
+
 }
 
+void ReleaseAll() {
+}
 
-ShaderManager::shader_raw ShaderManager::CreateShader(const std::string& tag,
-                                                      const opgs16::resource::SShader& container) {
+void ReleaseShader(const std::string& shader_name) {
+  if (DoesShaderExist(shader_name))
+    m_shaders.erase(shader_name);
+}
+
+shader_raw GetShader(const std::string& name) {
+  if (!DoesShaderExist(name)) {
+		auto& list = manager::resource::GetShader(name);
+		CreateShader(name, list);
+	}
+	return m_shaders[name].get();
+}
+
+} /// ::opgs16::manager::shader namespace.
+
+opgs16::manager::shader::shader_raw CreateShader(
+    const std::string& tag,
+    const opgs16::resource::SShader& container) {
 	if (DoesShaderExist(tag))
-        return Shader(tag);
+    return opgs16::manager::shader::GetShader(tag);
 
-	auto shader = std::make_unique<element::CShaderNew>();
+	auto shader = std::make_unique<opgs16::element::CShaderNew>();
 	for (auto& [type, shader_path] : container.List()) {
 		shader->SetShader(type, shader_path.c_str());
 	}
 	shader->Link();
 
-	/** Bind */
+	// Bind
 	m_shaders[tag] = std::move(shader);
 	return m_shaders[tag].get();
 }
 
-void ShaderManager::Clear() {
-    //m_shaders.clear();
+inline bool DoesShaderExist(const std::string& shader_name) {
+  return m_shaders.find(shader_name) != m_shaders.end();
 }
-
-void ShaderManager::ReleaseShader(const std::string& shader_name) {
-    if (DoesShaderExist(shader_name))
-        m_shaders.erase(shader_name);
-}
-
-} /*! opgs16::manager */
-
