fa48eda66df3bf1c8cfb5f880390e4519cadeb49
liliilli <jmyundev@gmail.com>
2018-06-15 18:08:50 +0900

    refc: Time manager refactoring

--- OPGS16/Include/Manager/time_manager.h
+++ OPGS16/Include/Manager/time_manager.h
@@ -1,146 +1,80 @@
 #ifndef OPGS16_SYSTEM_MANAGER_TIME_MANAGER_H
 #define OPGS16_SYSTEM_MANAGER_TIME_MANAGER_H
 
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Manager/time_manager.h
+///
+/// @brief Time manager manages time quantums, fps, and so forth.
+///
+/// @log
+/// 2018-02-10 Create file.
+/// 2018-03-03 Refactoring.
+/// 2018-06-15 Refactoring.
+///
 
-/*!
- * @file System/Manager/Public/time_manager.h
- * @brief Time manager manages time quantums, fps, and so forth.
- *
- * @log
- * 2018-02-10 Create file.
- * 2018-03-03 Refactoring.
- */
+/// ::phitos::enums::ESwitch
+#include <Phitos/Enums/switch.h>
 
-#include <GLFW\glfw3.h>
+///
+/// @namespace time
+///
+/// @brief Manages time quantum.
+///
+/// @author Jongmin Yun
+///
+/// @log
+/// 2018-03-03 Refactoring. Moved it to opgs16::manager namespace.
+/// 2018-06-15 Refactoring.
+///
+namespace opgs16::manager::time {
 
-namespace opgs16 {
-namespace manager {
+///
+/// @brief Update time tick for application.
+///
+void Update();
 
-/*!
- * @class MTimerManager
- * @brief Manages time quantum.
- * @author Jongmin Yun
- *
- * @log
- * 2018-03-03 Refactoring. Moved it to opgs16::manager namespace.
- */
-class MTimeManager final {
-public:
-    /*! Return static instance. */
-    static MTimeManager& Instance() {
-        static MTimeManager instance{};
-        return instance;
-    }
+///
+/// @brief Helper method that checks if or not next frame can be proceeded.
+/// @return If next frame is set, return true. else false.
+///
+bool Ticked();
 
-    /*!
-     * @brief
-     */
-    inline void Update() {
-        if (is_first) {
-            is_first = !is_first;
-            old_time = new_time = (float)glfwGetTime();
-        }
-        else {
-            new_time        = (float)glfwGetTime();
-            delta_time      = new_time - old_time;
-            old_time        = new_time;
-            elapsed_time   += delta_time;
-        }
-    }
+///
+/// @brief Get delta time how much time passed from previous frame.
+/// Format is integer part is seconds, and point part is millisecond region.
+///
+/// @return Float value delta time value.
+///
+float GetDeltaTime();
 
-    /*!
-     * @brief Helper method that checks if next frame set when V-sync is on.
-     * @return If next frame is set, return true. else false.
-     */
-    inline bool Ticked() {
-        if (elapsed_time >= interval) {
-            fps_second = 1 / elapsed_time;
-            elapsed_time -= interval;
-            return true;
-        }
-        else return false;
-    }
+///
+/// @brief Return present frame per second.
+///
+float GetPresentFps();
 
-    /*!
-     * @brief
-     * @return
-     */
-    inline const float GetDeltaTime() {
-        return interval;
-    }
+///
+/// @brief The methods set fps (frame per seconds);
+/// This methods can be called anytime use wants.
+/// But when v-sync is off, this methods would be useless.
+///
+/// @param[in] fps_value Fps tick count per second.
+///
+void SetFps(float fps_value);
 
-    /*!
-     * @brief
-     * @return
-     */
-    inline const float GetFpsSeconds() {
-        return fps_second;
-    }
+///
+/// @brief Set vsync mode.
+/// If vsync mode is off, application will be running by more 60 fps but unstable.
+/// If vsync mode is on, it will be running by your specified fps or 60 fps.
+///
+/// @param[in] mode Mode switch value.
+///
+void SetVsync(phitos::enums::ESwitch mode);
 
-    /*!
-     * @brief The methods set fps (frame per seconds);
-     * This methods can be called anytime use wants.
-     * But when v-sync is off, this methods would be useless.
-     *
-     * @param[in] value Fps tick count per second.
-     */
-     void SetFps(float value) {
-        interval = 1 / value;
-    }
-
-	/**
-	 * @brief
-	 */
-	inline const float GetDeltaTime() const {
-        return interval;
-    }
-
-private:
-    float old_time{};		/** Old time point of previous frame. */
-    float new_time{};		/** New time point of present frame. */
-    float elapsed_time{};	/** Total time from previous to new, used for checking frame tick. */
-    float delta_time{};	    /** Delta time calculated from new_time - old_time. */
-    float interval{};		/** frame tick interval time, used only v-sync is on. */
-    float fps_second{};	    /** Time value for displaying text when fps_toggled is true. */
-
-    bool is_first{ true };
-
-private:
-    MTimeManager() = default;
-
-public:
-    MTimeManager(const MTimeManager&) = delete;
-    MTimeManager(const MTimeManager&&) = delete;
-};
-
-} /*! opgs16::manager */
-} /*! opgs16 */
+} /// ::opgs16::manager::time namespace
 
 #endif // !OPGS16_SYSTEM_MANAGER_TIME_MANAGER_H
---
+++ OPGS16/Source/Manager/time_manager.cc
@@ -0,0 +1,118 @@
+
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Manager/time_manager.cc
+///
+/// @brief Time manager manages time quantums, fps, and so forth.
+///
+/// @log
+/// 2018-06-15 Create file.
+///
+
+/// Header file
+#include <Manager/time_manager.h>
+
+#include <GLFW/glfw3.h>
+/// ::phitos::enums::ESwitch
+#include <Phitos/Enums/switch.h>
+
+//!
+//! Datas
+//!
+
+namespace {
+using phitos::enums::ESwitch;
+
+// Old time point of previous frame.
+float old_time = 0.f;
+// New time point of present frame.
+float new_time = 0.f;
+// Total time from previous to new, used for checking frame tick.
+float elapsed_time = 0.f;
+
+/// Internal second calculation value.
+float __elapsed_second = 0.f;
+
+// Delta time of each tick.
+float delta_time = 0.f;
+// frame tick fixed_fps_interval time, used only v-sync is on.
+float fixed_fps_interval = 0.f;
+
+// Time value for displaying text when fps_toggled is true.
+float previous_fps_second = 0.f;
+float present_fps_second = 0.f;
+
+ESwitch is_first = ESwitch::Off;
+ESwitch is_vsync = ESwitch::On;
+
+} /// unnamed namespace
+
+//!
+//! Definition
+//!
+
+namespace opgs16::manager::time {
+
+void Update() {
+  if (is_first == ESwitch::Off) {
+    old_time = new_time = static_cast<float>(glfwGetTime());
+    is_first = !is_first;
+    return;
+  }
+
+  new_time = static_cast<float>(glfwGetTime());
+  const auto tick = new_time - old_time;
+
+  switch (is_vsync) {
+  case ESwitch::Off: elapsed_time = tick; break;
+  case ESwitch::On: elapsed_time += tick; break;
+  }
+
+  if (__elapsed_second += tick; __elapsed_second >= 1.0f) {
+    __elapsed_second -= 1.0f;
+    previous_fps_second = present_fps_second;
+    present_fps_second = 0.f;
+  }
+
+  old_time = new_time;
+}
+
+bool Ticked() {
+  if (is_vsync == ESwitch::Off) {
+    delta_time = elapsed_time;
+    present_fps_second += 1;
+    return true;
+  }
+
+  // Fixed frame per second below.
+  if (elapsed_time < fixed_fps_interval)
+    return false;
+
+  present_fps_second += 1;
+  delta_time    = fixed_fps_interval;
+  elapsed_time -= fixed_fps_interval;
+  return true;
+}
+
+float GetDeltaTime() {
+  return delta_time;
+}
+
+float GetPresentFps() {
+  return previous_fps_second;
+}
+
+void SetFps(float fps_value) {
+  fixed_fps_interval = 1 / fps_value;
+}
+
+void SetVsync(phitos::enums::ESwitch mode) {
+  is_vsync = mode;
+  elapsed_time = 0.f;
+}
+
+} /// ::opgs16::manager::time namespace
