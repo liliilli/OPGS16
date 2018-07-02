93e3a484d6610e5da12526959b54ab2e4b74fde0
liliilli <jmyundev@gmail.com>
2018-07-02 17:55:08 +0900

    refc: DVector UiObject

--- OPGS16/Include/Element/ui_object.h
+++ OPGS16/Include/Element/ui_object.h
@@ -1,96 +1,76 @@
 #ifndef OPGS16_SYSTEM_OBJECT_UI_OBJECT_H
 #define OPGS16_SYSTEM_OBJECT_UI_OBJECT_H
 
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*!
- * @file Element\ui_object.h
- * @brief Base clase for m_object_list inherit UiObject. (UI component)
- *
- * @author Jongmin Yun
- * @log
- * 2018-02-19 Remove Draw() override and Draw(ShaderNew) deleted method.
- * 2018-03-11 Correction of UiObject namespace hierarchy positioning, and path.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Element/ui_object.h
+/// @brief Base clase for m_object_list inherit UiObject. (UI component)
+///
+/// @author Jongmin Yun
+/// @log
+/// 2018-02-19 Remove Draw() override and Draw(ShaderNew) deleted method.
+/// 2018-03-11 Correction of UiObject namespace hierarchy positioning, and path.
+/// 2018-07-02 Refactring.
+///
 
 #include <GL/glew.h>
-#include <Element\object.h> /// ::opgs16::element::CObject
-#include <Element\Interface\i_originable.h> /// IOriginable
+/// ::opgs16::element::CObject
+#include <Element/object.h>
+/// ::opgs16::IOriginable
+#include <Element/Interface/i_originable.h>
 
-namespace opgs16 {
-namespace element {
+namespace opgs16::element {
 
-/*!
- * @class UiObject
- * @brief UiObject class would be used to implement Ui component, such as Canvas, Image, etc.
- * First, UiObject has slightly different Update mechanism, to display components hierarcherly.
- *
- * Second, UiObject has only a child inherits UiObject class.
- * Otherwise, undefined behavior will occur (I swear)
- */
+///
+/// @class UiObject
+/// @brief UiObject class would be used to implement Ui component, such as Canvas, Image, etc.
+/// First, UiObject has slightly different Update mechanism, to display components hierarcherly.
+///
+/// Second, UiObject has only a child inherits UiObject class.
+/// Otherwise, undefined behavior will occur (I swear)
+///
 class UiObject : public CObject, public IOriginable {
 public:
 	virtual ~UiObject() = default;
 
-    /*! Uiobject updates children edge position */
-    virtual void LocalUpdate() override;
+  /// Uiobject updates children edge position.
+  virtual void LocalUpdate() override;
 
 protected:
-	/**
-	 * @brief Updates screen_x, screen_y, width, height coordinate value in Screen space.
-	 * This methods updates caller's screen_x, y, width, height itself.
-	 * @param[in] parent_xywh Caller's screen_x,y,width,height position array.
-	 */
-	 void UpdateScreenXYWH(const std::array<GLint, 4> parent_xywh);
+	///
+	/// @brief Updates screen_x, screen_y, width, height coordinate value
+  /// in Screen space.
+	/// This methods updates caller's screen_x, y, width, height itself.
+  ///
+	/// @param[in] parent_xywh Caller's screen_x,y,width,height position array.
+	///
+	void UpdateScreenXYWH(const std::array<GLint, 4> parent_xywh);
 
 private:
-	float screen_x{}, screen_y{};
-	float screen_width{}, screen_height{};
+  DVector2 screen_origin = DVector2{};
+  DVector2 screen_size = DVector2{};
 
 private:
-	/**
-	 * @brief Updates callee(child)'s final position to be rendered.
-	 *
-	 * This method gets parent(Caller)'s x, y, width, height value of Screen space,
-	 * and calculate rendering position referring to IOriginable::Origin value of child.
-	 * Detail mechanism is below.
-	 * \(7)|(8)/(9) At first, method gets Origin value of child which is left and calculate
-	 * -(4).(5)-(6) with parent_x, y, width, height to be aligned Parent's region without
-	 * /(1)|(2)\(3) applying child's local position.
-	 * Afterward, this method sets final rendering position of child and apply changed value to
-	 * children of child calling SetUiParentPosition() of children of child.
-	 */
-	 void SetUiParentPosition(const float parent_x, const float parent_y,
-                              const float parent_width, const float parent_height);
+	///
+	/// @brief Updates callee(child)'s final position to be rendered.
+	///
+	/// This method gets parent(Caller)'s x, y, width, height value of Screen space,
+	/// and calculate rendering position referring to IOriginable::Origin value of child.
+	/// Detail mechanism is below.
+	/// \(7)|(8)/(9) At first, method gets Origin value of child which is left and calculate
+	/// -(4).(5)-(6) with parent_x, y, width, height to be aligned Parent's region without
+	/// /(1)|(2)\(3) applying child's local position.
+	/// Afterward, this method sets final rendering position of child and apply changed value to
+	/// children of child calling SetUiParentPosition() of children of child.
+	///
+	void SetUiParentPosition(float parent_x, float parent_y,
+                           float parent_width, float parent_height);
 };
 
-} /*! opgs16::element */
-} /*! opgs16 */
+} /// ::opgs16::element namespace
 
-#endif /** OPGS16_SYSTEM_OBJECT_UI_OBJECT_H */
+#endif /// OPGS16_SYSTEM_OBJECT_UI_OBJECT_H
--- OPGS16/Source/Element/ui_object.cc
+++ OPGS16/Source/Element/ui_object.cc
@@ -1,82 +1,68 @@
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-/*!
- * @file System/Element/Private/ui_object.cc
- * @log
- * 2018-03-11 Add boilerplate comment, replace it within ::opgs16::element.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Element/ui_object.cc
 
-#include <Element\ui_object.h>  /// Header file
+/// @log
+/// 2018-03-11 Add boilerplate comment, replace it within ::opgs16::element.
+/// 2018-07-02 Refactring.
+///
 
-#include <array>                /// std::array
+/// Header file
+#include <Element/ui_object.h>
 
-#include <glm\glm.hpp>
+#include <array>
+/// std::array
 
-namespace opgs16 {
-namespace element {
+#include <glm/glm.hpp>
+
+namespace opgs16::element {
 
 void UiObject::LocalUpdate() {
-    for (auto& child : m_children) {
-        if (child.second && child.second->GetActive()) {
-            auto child_temp = static_cast<UiObject*>(child.second.get());
-            child_temp->SetUiParentPosition(screen_x, screen_y, screen_width, screen_height);
-        }
+  for (auto& child : m_children) {
+    if (child.second && child.second->GetActive()) {
+      auto child_temp = static_cast<UiObject*>(child.second.get());
+      child_temp->SetUiParentPosition(screen_origin.x, screen_origin.y,
+                                      screen_size.x, screen_size.y);
     }
+  }
 }
 
-void UiObject::SetUiParentPosition(const float parent_x, const float parent_y,
-                                   const float parent_width, const float parent_height) {
-	auto origin	= static_cast<int>(GetOrigin()) - 1;
-	unsigned x = origin % 3;
-	unsigned y = origin / 3;
+void UiObject::SetUiParentPosition(
+    const float parent_x,
+    const float parent_y,
+    const float parent_width,
+    const float parent_height) {
+  const auto origin	= static_cast<int>(GetOrigin()) - 1;
+	const int32_t x = origin % 3;
+	const int32_t y = origin / 3;
 
-	GLint source_x = static_cast<GLint>(parent_x + (parent_width / 2) * x);
-	GLint source_y = static_cast<GLint>(parent_y + (parent_height / 2) * y);
+	const auto source_x = static_cast<int32_t>(parent_x + (parent_width / 2) * x);
+	const auto source_y = static_cast<int32_t>(parent_y + (parent_height / 2) * y);
 
   SetParentPosition( DVector3{
       static_cast<float>(source_x),
       static_cast<float>(source_y),
       0.f }
   );
+
 	for (auto& child : GetChildList()) {
 		/** TODO :: NEED PERFORMANCE CHECK */
 		auto child_temp = static_cast<UiObject*>(child.second.get());
-		child_temp->SetUiParentPosition(screen_x, screen_y, screen_width, screen_height);
+    child_temp->SetUiParentPosition(screen_origin.x, screen_origin.y,
+                                    screen_size.x, screen_size.y);
 	}
 }
 
 void UiObject::UpdateScreenXYWH(const std::array<GLint, 4> xywh) {
-	screen_x		= static_cast<float>(xywh[0]);
-	screen_y		= static_cast<float>(xywh[1]);
-	screen_width	= static_cast<float>(xywh[2]);
-	screen_height	= static_cast<float>(xywh[3]);
+	screen_origin.x = static_cast<float>(xywh[0]);
+	screen_origin.y = static_cast<float>(xywh[1]);
+	screen_size.x   = static_cast<float>(xywh[2]);
+	screen_size.y   = static_cast<float>(xywh[3]);
 }
 
-} /*! opgs16::element */
-} /*! opgs16 */
+} /// ::opgs16::element namespace
