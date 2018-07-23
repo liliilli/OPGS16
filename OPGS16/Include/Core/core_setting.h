94d234791a4fdbf6233c92d0841613192f046f24
liliilli <jmyundev@gmail.com>
2018-07-23 22:15:30 +0900

    fix: SGlobalSetting refactoring and move to namespace

--- OPGS16/Include/Core/core_setting.h
+++ OPGS16/Include/Core/core_setting.h
@@ -1,165 +1,84 @@
 #ifndef OPGS16_SYSTEM_CORE_CORE_SETTING_H
 #define OPGS16_SYSTEM_CORE_CORE_SETTING_H
 
-/*!
- * @license BSD 2-Clause License
- *
- * Copyright (c) 2018, Jongmin Yun(Neu.)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+///
+/// @license BSD 2-Clause License
+///
+/// Copyright (c) 2018, Jongmin Yun(Neu.), All rights reserved.
+/// If you want to read full statements, read LICENSE file.
+///
+/// @file Core/core_setting.h
+///
+/// @brief Have been written about application global setting components and management class.
+///
+/// @author Jongmin Yun
+///
+/// @log
+/// 2018-03-01 Create file.
+/// 2018-07-23 Version up! 0.2.0
+///
 
-/*!
- * @file Core/core_setting.h
- * @brief Have been written about application global setting components and management class.
- * @author Jongmin Yun
- *
- * @log
- * 2018-03-01 Create file.
- */
-
-#include <array>        /*! std::array */
-#include <type_traits>  /*! std::underlying_type<> */
-
-#include <Helper\switch.h>  /// ::opgs16::Switch
+#include <array>
+/// ::opgs16::Switch
+#include <Helper/switch.h>
 
 #define _OPGS16_VERSION_MAJOR 0
-#define _OPGS16_VERSION_MINOR 1
+#define _OPGS16_VERSION_MINOR 2
 #define _OPGS16_VERSION_FIXED 0
-#define _OPGS16_CODENAME Dragonload
+#define _OPGS16_CODENAME Malroth
 
-namespace opgs16 {
-
-enum class EScaleType : unsigned {
-    X1 = 1, /*! Screen will be shown with screen size */
-    X2 = 2,	/*! Screen will be shown with double of screen size */
-    X3 = 3,	/*! Screen will be shown with triple of screen size */
+enum class EScaleType : int32_t {
+  X1 = 1, /// Screen will be shown with screen size.
+  X2 = 2,	/// Screen will be shown with double of screen size.
+  X3 = 3,	/// Screen will be shown with triple of screen size.
 };
 
-/*! Check and return boolean value whether lhs is same to rhs. */
-inline constexpr bool IsSameValue(EScaleType lhs, EScaleType rhs) {
-    return lhs == rhs;
-}
+namespace opgs16::setting {
 
-/*!
- * @class SGlobalSetting
- * @brief Storage of application global setting. these setting options are provided by default,
- * so user could not insert new option or remove existed option.
- *
- * Example : get screen size or either width or height.
- *     auto var = opgs16::SGlobalSetting::ScreenSize();
- *     auto var = opgs16::SGlobalSetting::ScreenWidth();
- *     auto var = opgs16::SGlobalSetting::ScreenHeight();
- */
-class SGlobalSetting final {
-private:
-    using size_type = std::array<unsigned, 2>;
+using TActualScreenSize = std::array<int32_t, 2>;
 
-public:
-    constexpr static size_type ScreenSize() noexcept {
-        return screen_size;
-    }
-    constexpr static size_type::value_type ScreenWidth() noexcept {
-        return screen_size[0];
-    }
-    constexpr static size_type::value_type ScreenHeight() noexcept {
-        return screen_size[1];
-    }
+bool IsEnableAntiAliasing() noexcept;
 
-    inline Switch AntiAliasing() const noexcept {
-        return m_anti_aliasing;
-    }
-    inline Switch DebugMode() const noexcept {
-        return m_debug_mode;
-    }
-    inline Switch PostProcessing() const noexcept {
-        return m_post_processing;
-    }
-    inline Switch SizeScalable() const noexcept {
-        return m_size_scalable;
-    }
-    inline EScaleType ScaleValue() const noexcept {
-        return m_scale_value;
-    }
-    inline Switch CollisionAABBBoxDisplay() const noexcept {
-      return m_collision_aabb_box;
-    }
-    inline auto ScaleValueIntegerOf() const noexcept {
-        return static_cast<std::underlying_type_t<decltype(m_scale_value)>>(m_scale_value);
-    }
+bool IsEnableDebugMode() noexcept;
 
-    inline void SetAntiAliasing(Switch value) noexcept {
-        m_anti_aliasing = value;
-    }
-    inline void SetDebugMode(Switch value) noexcept {
-        m_debug_mode = value;
-    }
-    inline void SetPostProcessing(Switch value) noexcept {
-        m_post_processing = value;
-    }
-    inline void SetSizeScalable(Switch value) noexcept {
-        m_size_scalable = value;
-    }
-    inline void SetScaleValue(EScaleType value) noexcept {
-        m_scale_value = value;
-    }
-    inline void SetCollisionAABBBoxDisplay(Switch value) noexcept {
-      m_collision_aabb_box = value;
-    }
+bool IsEnablePostProcessing() noexcept;
 
-    inline void ToggleAntiAliasing() noexcept {
-        SetAntiAliasing(ToggleSwitch(AntiAliasing()));
-    }
-    inline void ToggleDebugMode() noexcept {
-        SetDebugMode(ToggleSwitch(DebugMode()));
-    }
-    inline void TogglePostProcessing() {
-        SetPostProcessing(ToggleSwitch(PostProcessing()));
-    }
-    inline void ToggleSizeScalable() {
-        SetSizeScalable(ToggleSwitch(SizeScalable()));
-    }
-    inline void ToggleCollisionAABBBoxDisplay() noexcept {
-      SetCollisionAABBBoxDisplay(ToggleSwitch(CollisionAABBBoxDisplay()));
-    }
+bool IsEnableScaling() noexcept;
 
-private:
-    constexpr static size_type screen_size{ 256, 224 };
+bool IsEnableRenderingAabb() noexcept;
 
-    Switch m_anti_aliasing{ false };
-    Switch m_debug_mode{ false };
-    Switch m_post_processing{ false };
-    Switch m_size_scalable{ true };
-    Switch m_collision_aabb_box = Switch::OFF;
-    EScaleType m_scale_value{ EScaleType::X1 };
+void SetAntiAliasing(Switch value) noexcept;
 
-public:
-    SGlobalSetting();
-    SGlobalSetting(const SGlobalSetting&) = delete;
-    SGlobalSetting operator=(const SGlobalSetting&) = delete;
-};
+void SetDebugMode(Switch value) noexcept;
 
-} /*! opgs16 */
+void SetPostProcessing(Switch value) noexcept;
+
+void SetSizeScalable(Switch value) noexcept;
+
+void SetScaleValue(EScaleType value) noexcept;
+
+void SetCollisionAABBBoxDisplay(Switch value) noexcept;
+
+void ToggleAntiAliasing() noexcept;
+
+void ToggleDebugMode() noexcept;
+
+void TogglePostProcessing();
+
+void ToggleSizeScalable();
+
+void ToggleCollisionAABBBoxDisplay() noexcept;
+
+const TActualScreenSize& GetScreenSize() noexcept;
+
+TActualScreenSize::value_type GetScreenWidth() noexcept;
+
+TActualScreenSize::value_type GetScreenHeight() noexcept;
+
+int32_t GetScaleValueIntegerOf() noexcept;
+
+EScaleType GetScaleValue() noexcept;
+
+} /// ::opgs16::setting namespace
 
 #endif
-
--- OPGS16/Source/Core/core_setting.cc
+++ OPGS16/Source/Core/core_setting.cc
@@ -6,12 +6,123 @@
 /// If you want to read full statements, read LICENSE file.
 ///
 
-#include <Core\core_setting.h>  /// Header file
+/// Header file
+#include <Core/core_setting.h>
+#include <../manifest.h>
 
-namespace opgs16 {
+namespace {
+using opgs16::setting::TActualScreenSize;
 
-SGlobalSetting::SGlobalSetting() {
+bool m_anti_aliasing_feature    = false;
+bool m_is_debug_mode            = false;
+bool m_post_processing_feature  = false;
+bool m_scaling_feature          = true;
+bool m_render_collision_aabb    = false;
 
+EScaleType m_scale_value = EScaleType::X1;
+
+#if defined(OP16_SETTING_RESOLUTION_256224)
+  #if defined(OP16_SETTING_RESOLUTION_320240) ||\
+      defined(OP16_SETTING_RESOLUTION_640480)
+    static_assert(false, "Just only one resolution setting option must be turned on.");
+  #else
+    constexpr static TActualScreenSize screen_size{ 256, 224 };
+  #endif
+#elif defined(OP16_SETTING_RESOLUTION_320240)
+  #if defined(OP16_SETTING_RESOLUTION_256224) ||\
+      defined(OP16_SETTING_RESOLUTION_640480)
+    static_assert(false, "Just only one resolution setting option must be turned on.");
+  #else
+    constexpr static TActualScreenSize screen_size{ 320, 240 };
+  #endif
+#elif defined(OP16_SETTING_RESOLUTION_640480)
+  #if defined(OP16_SETTING_RESOLUTION_256224) ||\
+      defined(OP16_SETTING_RESOLUTION_320240)
+    static_assert(false, "Just only one resolution setting option must be turned on.");
+  #else
+    constexpr static TActualScreenSize screen_size{ 640, 480 };
+  #endif
+#else
+  static_assert(false, "At least one resolution setting option must be turned on.")
+#endif
+
+} /// ::unnamed namesapce
+
+namespace opgs16::setting {
+
+bool IsEnableAntiAliasing() noexcept {
+  return m_anti_aliasing_feature;
+}
+
+bool IsEnableDebugMode() noexcept {
+  return m_is_debug_mode;
+}
+
+bool IsEnablePostProcessing() noexcept {
+  return m_post_processing_feature;
+}
+
+bool IsEnableScaling() noexcept {
+  return m_scaling_feature;
+}
+
+bool IsEnableRenderingAabb() noexcept {
+  return m_render_collision_aabb;
+}
+
+EScaleType GetScaleValue() noexcept {
+  return m_scale_value;
+}
+
+int32_t GetScaleValueIntegerOf() noexcept {
+  return static_cast<int32_t>(m_scale_value);
+}
+
+void SetAntiAliasing(Switch value) noexcept {
+  m_anti_aliasing_feature = value == Switch::ON;
+}
+void SetDebugMode(Switch value) noexcept {
+  m_is_debug_mode = value == Switch::ON;
+}
+void SetPostProcessing(Switch value) noexcept {
+  m_post_processing_feature = value == Switch::ON;
+}
+void SetSizeScalable(Switch value) noexcept {
+  m_scaling_feature = value == Switch::ON;
+}
+void SetScaleValue(EScaleType value) noexcept {
+  m_scale_value = value;
+}
+void SetCollisionAABBBoxDisplay(Switch value) noexcept {
+  m_render_collision_aabb = value == Switch::ON;
+}
+
+void ToggleAntiAliasing() noexcept {
+  SetAntiAliasing(ToggleSwitch(Switch{IsEnableAntiAliasing()}));
+}
+void ToggleDebugMode() noexcept {
+  SetDebugMode(ToggleSwitch(Switch{IsEnableDebugMode()}));
+}
+void TogglePostProcessing() {
+  SetPostProcessing(ToggleSwitch(Switch{IsEnablePostProcessing()}));
+}
+void ToggleSizeScalable() {
+  SetSizeScalable(ToggleSwitch(Switch{IsEnableScaling()}));
+}
+void ToggleCollisionAABBBoxDisplay() noexcept {
+  SetCollisionAABBBoxDisplay(ToggleSwitch(Switch{IsEnableRenderingAabb()}));
+}
+
+const TActualScreenSize& GetScreenSize() noexcept {
+  return screen_size;
+}
+
+TActualScreenSize::value_type GetScreenWidth() noexcept {
+  return screen_size[0];
+}
+
+TActualScreenSize::value_type GetScreenHeight() noexcept {
+  return screen_size[1];
 }
 
 } /// ::opgs16
