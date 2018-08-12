d95c3e99d8e0cb92444ddbae036c47e4ff2507a2
liliilli <jmyundev@gmail.com>
2018-08-13 03:38:48 +0900

    feat: Add Front, Right, UP vector

--- OPGS16/Include/Helper/Type/vector3.h
+++ OPGS16/Include/Helper/Type/vector3.h
@@ -363,6 +363,30 @@ public:
   static bool IsAllZero(const opgs16::DVector3& vector) noexcept;
 
   ///
+  /// @brief Return {0, 0, 1} front DVector3 vector.
+  ///
+  static DVector3 FrontZ() noexcept {
+    static DVector3 ret{0, 0, 1};
+    return ret;
+  }
+
+  ///
+  /// @brief Return {1, 0, 0} right DVector3 vector.
+  ///
+  static DVector3 RightX() noexcept {
+    static DVector3 ret{1, 0, 0};
+    return ret;
+  }
+
+  ///
+  /// @brief Return {0, 1, 0} up DVector3 vector.
+  ///
+  static DVector3 UpY() noexcept {
+    static DVector3 ret{0, 1, 0};
+    return ret;
+  }
+
+  ///
   /// @brief Check if this DVector3 is all zero or nearly equal to zero.
   ///
   bool IsAllZero() const noexcept;
